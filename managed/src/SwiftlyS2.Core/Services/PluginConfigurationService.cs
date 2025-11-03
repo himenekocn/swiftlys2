425d7419940d20f54295b37d59372242933f7505
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-11-03 17:32:47 +0200

    refactor: use Tomlyn's built-in serialization instead of manual reflection
    - Replace custom TomlModelHelper class with Toml.FromModel()
    - Remove 117 lines of manual reflection-based TOML conversion
    - Simplify InitializeTomlWithModel to use native Tomlyn serialization
    - Clean up unused imports (System.Diagnostics.CodeAnalysis, System.Reflection, Tomlyn.Model)
    - Improve code maintainability and reliability

--- managed/src/SwiftlyS2.Core/Services/PluginConfigurationService.cs
+++ managed/src/SwiftlyS2.Core/Services/PluginConfigurationService.cs
@@ -1,131 +1,12 @@
 using System.Data.Common;
-using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
 using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 using SwiftlyS2.Core.Natives;
 using SwiftlyS2.Core.Services;
 using SwiftlyS2.Shared.Services;
 using Tomlyn;
-using Tomlyn.Model;
 
 namespace SwiftlyS2.Core.Services;
-
-internal static class TomlModelHelper
-{
-  [UnconditionalSuppressMessage("Trimming", "IL2075", Justification = "Configuration models are preserved at runtime")]
-  public static TomlTable ObjectToTomlTable(object obj)
-  {
-    var table = new TomlTable();
-    var properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-    foreach (var prop in properties)
-    {
-      var value = prop.GetValue(obj);
-      if (value == null)
-        continue;
-
-      var propName = prop.Name;
-
-      // Convert property value to TOML-compatible type
-      if (IsSimpleType(prop.PropertyType))
-      {
-        table[propName] = value;
-      }
-      else if (IsGenericDictionary(prop.PropertyType))
-      {
-        // Handle Dictionary<string, TValue>
-        var dictTable = new TomlTable();
-        if (value is System.Collections.IDictionary dictionary)
-        {
-          foreach (System.Collections.DictionaryEntry entry in dictionary)
-          {
-            if (entry.Key != null && entry.Value != null)
-            {
-              var key = entry.Key.ToString();
-              if (key != null)
-              {
-                if (IsSimpleType(entry.Value.GetType()))
-                {
-                  dictTable[key] = entry.Value;
-                }
-                else
-                {
-                  dictTable[key] = ObjectToTomlTable(entry.Value);
-                }
-              }
-            }
-          }
-        }
-        table[propName] = dictTable;
-      }
-      else if (prop.PropertyType.IsArray || IsGenericList(prop.PropertyType))
-      {
-        // Arrays or lists
-        var array = new TomlArray();
-        if (value is System.Collections.IEnumerable enumerable)
-        {
-          foreach (var item in enumerable)
-          {
-            if (item != null)
-            {
-              if (IsSimpleType(item.GetType()))
-              {
-                array.Add(item);
-              }
-              else
-              {
-                array.Add(ObjectToTomlTable(item));
-              }
-            }
-          }
-        }
-        table[propName] = array;
-      }
-      else if (prop.PropertyType.IsClass && !prop.PropertyType.IsArray)
-      {
-        // Nested object
-        table[propName] = ObjectToTomlTable(value);
-      }
-    }
-
-    return table;
-  }
-
-  private static bool IsSimpleType(Type type)
-  {
-    return type.IsPrimitive
-           || type.IsEnum
-           || type == typeof(string)
-           || type == typeof(decimal)
-           || type == typeof(DateTime)
-           || type == typeof(DateTimeOffset)
-           || type == typeof(TimeSpan)
-           || type == typeof(Guid);
-  }
-
-  private static bool IsGenericList(Type type)
-  {
-    if (!type.IsGenericType)
-      return false;
-
-    var genericTypeDef = type.GetGenericTypeDefinition();
-    return genericTypeDef == typeof(List<>)
-           || genericTypeDef == typeof(IList<>)
-           || genericTypeDef == typeof(ICollection<>)
-           || genericTypeDef == typeof(IEnumerable<>);
-  }
-
-  private static bool IsGenericDictionary(Type type)
-  {
-    if (!type.IsGenericType)
-      return false;
-
-    var genericTypeDef = type.GetGenericTypeDefinition();
-    return genericTypeDef == typeof(Dictionary<,>)
-           || genericTypeDef == typeof(IDictionary<,>);
-  }
-}
 
 internal class PluginConfigurationService : IPluginConfigurationService
 {
@@ -244,12 +125,12 @@ internal class PluginConfigurationService : IPluginConfigurationService
 
     var config = new T();
 
-    // Create a TomlTable and add the section
-    var tomlTable = new TomlTable();
-    var sectionTable = TomlModelHelper.ObjectToTomlTable(config);
-    tomlTable[sectionName] = sectionTable;
+    var wrapped = new Dictionary<string, object?>
+    {
+      [sectionName] = config
+    };
 
-    var tomlString = Toml.FromModel(tomlTable);
+    var tomlString = Toml.FromModel(wrapped);
     File.WriteAllText(configPath, tomlString);
 
     return this;
